f01a31e5e9f55b06e7e1f3709f6d2391974825d6
uuzjin <lyj5721@sookmyung.ac.kr>
2026-07-08 00:18:56 +0900

    Feat: 9주차 실습 내용

---
+++ 임유진/9주차 실습/EndScreen.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+
+
+    void Awake()
+    {
+        scoreKeeper = FindAnyObjectByType<ScoreKeeper>();
+        
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\nYou got a score of " + scoreKeeper.CalculateScore() + "%";
+    }
+}
